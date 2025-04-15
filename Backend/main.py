3f4a73cf90f2d6f95c80a92d03aa32aea35b8b41
Micker <carbonellerick811@gmail.com>
2025-04-15 17:37:28 -0600

    First Backend

---
+++ Backend/main.py
@@ -0,0 +1,42 @@
+def main():
+    pesosYLetras = []
+    ordenar(pesosYLetras)
+
+    for nombre in pesosYLetras:
+        print(nombre)
+
+if __name__ == "__main__":
+    main()
+
+def ordenar(lista):
+    tamanoDeLista = len(lista)
+    if tamanoDeLista > 0:
+        quick_sort(lista, 0, tamanoDeLista - 1)
+
+def quick_sort(lista, inicio, fin):
+    if inicio > fin:
+        return
+    anterior = inicio
+    posterior = fin
+    pivote = lista[inicio]
+
+    while anterior < posterior:
+        while anterior < posterior and lista[posterior] > pivote:
+            posterior = posterior - 1
+
+        if anterior < posterior:
+            lista[anterior] = lista[posterior]
+            anterior = anterior + 1
+
+        while anterior < posterior and lista[anterior] <= pivote:
+            anterior = anterior + 1
+
+        if anterior < posterior:
+            lista[posterior] = lista[anterior]
+            posterior = posterior - 1
+
+        lista[anterior] = pivote
+
+    quick_sort(lista, inicio, anterior - 1)
+    quick_sort(lista, anterior + 1, fin)
+
